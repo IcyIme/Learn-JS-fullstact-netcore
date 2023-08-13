572c2df18d1cb4c2d1ef7752695bf84901230e1f
IcyIme <peter.odrobinak@outlook.sk>
2023-08-13 17:11:41 +0200

    operation

---
+++ operations.js
@@ -0,0 +1,26 @@
+console.log(1 + 2); // 3
+console.log(6 - 3); // 3
+console.log(5 + 6 -1); // 10
+console.log(3*2); // 6
+console.log(42/7);
+console.log(5+2*3);  //2*3 will be calculated first
+//5+2 will be calculated first
+console.log((5+2)*3);
+
+/*Simple Operations
+Ever wondered how many seconds there are in a month (30 days)?
+
+Write a program to calculate and output the answer.
+
+
+Hint
+Remember, there are 24 hours in a day, 60 minutes in an hour, and 60 seconds in a minute.*/
+
+/* 30 days
+1 day = 24 hours
+1 hour = 60 minutes
+1 minute = 60 seconds */
+
+//your code goes here
+
+console.log(((30*24)*60)*60);
